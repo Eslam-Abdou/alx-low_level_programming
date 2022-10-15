3da4bec4df68dceecc26fa5f91011fedaa06546b
Eslam-Abdou <111107661+Eslam-Abdou@users.noreply.github.com>
2022-10-15 04:08:11 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 if no error, non zero if error.
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
